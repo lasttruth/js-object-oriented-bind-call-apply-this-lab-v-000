86e0360fbeef8e758c808e0b4979730a2c8fd3e8
Robin Doirin <robindoirin@gmail.com>
2019-06-27 15:05:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,3 +9,6 @@ function setThisWithCall(fn, thisValue, arg) {
  function setThisWithApply(fn, thisValue, arg) {
    return fn.apply(thisValue, arg);
  }
+ function returnNewFunctionOf(functionToBeCopied, thisValue) {
+   return functionToBeCopied.bind(thisValue);
+ }
